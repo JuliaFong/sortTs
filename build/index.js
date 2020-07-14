7e9ba7240abb16e3d0a376f7e7eec151323e77b9
Julia Fong <Juliamzfong@gmail.com>
2020-07-14 17:22:40 -0500

    Update bubble sort

--- build/index.js
+++ build/index.js
@@ -5,12 +5,24 @@ var Sorter = /** @class */ (function () {
     }
     Sorter.prototype.sort = function () {
         var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+        // This only works if collection is numbers[]
+        // If collection is an array of numbers
+        // Use a type guard to restore access to a specific type 
+        // in a union operator
+        if (this.collection instanceof Array) {
+            for (var i = 0; i < length; i++) {
+                for (var j = 0; j < length - i - 1; j++) {
+                    // Collection === number []
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // Only works if collection is a string
+                //If collection is a string, do this logic instead:
+                // ~~logic to compare and swap characters in a stringg
+                if (typeof this.collection === "string") {
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 class Sorter {
-    constructor(public collection: number[]) {}
+    constructor(public collection: number[] | string) {}
 
     sort(): void {
         const { length }= this.collection;
+        // This only works if collection is numbers[]
+        // If collection is an array of numbers
+        // Use a type guard to restore access to a specific type 
+        // in a union operator
 
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if(this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j]
-                    this.collection[j] = this.collection[j+1]
-                    this.collection[j+1] = leftHand
+        if (this.collection instanceof Array){
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < length - i - 1; j++) {
+                    // Collection === number []
+                    if(this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j]
+                        this.collection[j] = this.collection[j+1]
+                        this.collection[j+1] = leftHand
+                    }
+
+        }
+
+                // Only works if collection is a string
+                //If collection is a string, do this logic instead:
+                // ~~logic to compare and swap characters in a stringg
+                if (typeof this.collection === "string") {
+                    
                 }
             }
         }
